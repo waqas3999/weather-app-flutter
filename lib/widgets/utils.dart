d8117a85db68b94bac694cc25f60d48a6e0c8c1e
waqas <waqasusman3999@gmail.com>
2022-06-26 12:42:22 +0500

    - Initial commit

---
+++ lib/widgets/utils.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+///Colors
+Color app_Blackcolor=Color(0xFF000000);
+Color app_Greycolor=Color(0xFF868889);
+Color app_Greencolor=Color(0xFF6CC51D);
+Color app_Whitecolor=Color(0xFFFFFFFF);
+Color app_orangecolor=Color(0xFFE8AD41);
+Color app_Bluecolor=Color(0xFF407EC7);
+Color app_WhiteSeccolor=Color(0xFFF4F5F9);
+Color app_Redcolor=Color(0xFFEF574B);
+Color app_GreyDarkcolor=Color(0xFFB1B1B1);
+
+/////App TextStyles
+//Splash heading welcome
+TextStyle heading1=GoogleFonts.poppins(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    color: app_Blackcolor,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 30*( 3 / 100)
+);
+///POWERED bY
+TextStyle heading2=GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: app_Greycolor,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 15*( 20 / 100)
+);
+//Tech idara
+TextStyle heading3=GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: app_Greencolor,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 20*( 30.5 / 100)
+);
+//welcome
+TextStyle heading4=GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: app_Whitecolor,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 18*( 3 / 100)
+);
+//App paragraph lorem ispum
+TextStyle paragraph1=GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: app_Greycolor,
+    fontStyle: FontStyle.normal,
+    letterSpacing: 15*(3 / 100)
+);
+
+
+//App text Content without heading
+
