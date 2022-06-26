c7a66d853373db6f5f7c2a6c1583b509bd36beeb
waqas <waqasusman3999@gmail.com>
2022-06-26 12:34:17 +0500

    - Initial commit

---
+++ lib/screen/landing.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screen/landing.dart';
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
@@ -14,21 +15,21 @@ class _SplashState extends State<Splash> {
   }
   _navigateToLogin()async{
     await Future.delayed(Duration(seconds: 3),(){});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Login()));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Landing()));
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Container(
+      Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       decoration: BoxDecoration(
         image: DecorationImage(
           image:AssetImage("images/splash.png",),
           fit: BoxFit.fill,
-          )
+        )))
         ],
       ),
     );
